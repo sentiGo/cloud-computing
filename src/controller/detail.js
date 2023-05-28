e284349f60d1b070504341c55bc724bf684cb3d1
NaufalAssyafiqF <assyafiqnaufal625@gmail.com>
2023-05-28 19:10:44 +0700

    menambahkan API path /detail (nambahin try catch)

--- src/controller/detail.js
+++ src/controller/detail.js
@@ -1,23 +1,31 @@
 const connection = require("../config/database");
 
 
-const detail = async (req,res) => {
+const detail = async (req, res) => {
     const { id } = req.params;
-    
-    const query = `SELECT * FROM sby_data WHERE id='${id}'`
-    const [rows] = await connection.query(query);
 
-    const baseUrl = "https://storage.googleapis.com/capstone-project/imgDestinasi/";
+    try {
+        //query untuk mengambil data bersdarkan id
+        const query = `SELECT * FROM sby_data WHERE id='${id}'`
+        const [rows] = await connection.query(query);
+
+        const baseUrl = "https://storage.googleapis.com/capstone-project/imgDestinasi/";
 
 
-    //untuk memsaukan url gambar destinasi ke masing2 data
-    const detail = rows.map((item) => {
-        const imgUrl = `${baseUrl}${item.kota}/${item.id}`;
-        const rating = parseFloat(item.rating);
-        return { ...item, img: imgUrl, rating};
-    });
+        //untuk memsaukan url gambar destinasi ke masing2 data
+        const detail = rows.map((item) => {
+            const imgUrl = `${baseUrl}${item.kota}/${item.id}`;
+            const rating = parseFloat(item.rating);
+            return { ...item, img: imgUrl, rating };
+        });
 
-    res.send(detail)
+        res.send(detail)
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: true, message: 'Internal Server Error' });
+    }
+
+
 }
 
 
